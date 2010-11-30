329d5e3c7c066a25ef72fbc7b43b7dd00769cc0e
leandro.aless <leandro.aless@4a32209d-7bc5-8c5b-463c-d9bb2390e756>
2010-11-30 22:13:57 +0000

    Hago un commit por si las moscas...

---
+++ trunk/Acoplamiento de Programas/src/detectorDeAcoplamiento/GrafoReverso.java
@@ -0,0 +1,48 @@
+package detectorDeAcoplamiento;
+
+import java.util.ArrayList;
+
+import detectorDeAcoplamiento.Grafo;
+import detectorDeAcoplamiento.NodoGrafo;
+
+public class GrafoReverso {
+	
+	private int cantidadDePaquetes;
+	private NodoGrafo[] nodos;
+	
+	public GrafoReverso(int cantidadDePaquetes){
+		this.cantidadDePaquetes = cantidadDePaquetes;
+	}
+
+	/**
+	 * Calcula, dado un grafo, su reverso.
+	 * @param grafo
+	 */
+	public void calcularGrafoReverso(Grafo grafo){
+		
+		int tamanio = grafo.getCantidadDePaquetes();
+		NodoGrafo[] nodos = grafo.componentesDelGrafo();
+		GrafoReverso grafoReverso = new GrafoReverso(tamanio);
+		NodoGrafo[] nodoReverso = grafoReverso.getNodos();
+		
+		for(int i = 0; i < tamanio; i++){  //reocorro grafo original y obtengo la lista de adyacencia de cada nodo
+			ArrayList listaDeAdyacencia = nodos[i].getListaDeAdyacencia();
+			int id = nodos[i].getIDinterno();
+			
+			if ( !listaDeAdyacencia.isEmpty()){
+				
+				for(int j = 0; j < listaDeAdyacencia.size(); j++){  //recorro la lista de adyacencia del nodo
+					Integer nodoAdyacente = (Integer)listaDeAdyacencia.get(j);
+					nodoReverso[j].setIDinterno(nodoAdyacente);
+					ArrayList listaDeAdyacenciaReversa = nodoReverso[j].getListaDeAdyacencia();
+					listaDeAdyacenciaReversa.add(id);
+				}
+			}
+		}
+	}
+
+	private NodoGrafo[] getNodos() {
+		return nodos;
+	}
+	
+}
